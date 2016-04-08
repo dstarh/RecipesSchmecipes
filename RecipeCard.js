4f207df533e1ef73ee0898c60c7c4e6296ef5567
David Herman <dstarh@purpleasphalt.com>
2016-04-08 16:20:18 -0400

    master mroe recipe card goodness

--- RecipeCard.js
+++ RecipeCard.js
@@ -14,12 +14,15 @@ var {
 var {height, width} = Dimensions.get('window');
 var ApiKeys = require('./ApiKeys')
 var getImageSource = require('./getImageSource');
-
+var entities = require('entities')
 var API_URL = "https://api.nutrio.com"
 var RecipeCard = React.createClass({
   getInitialState: function() {
     return {
       ingredientDataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      prepNotesDataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       })
     }
@@ -28,7 +31,7 @@ var RecipeCard = React.createClass({
     this.fetchFullReicpe(this.props.recipe.guid);
   },
   getRecipeEndpoint: function(guid: string){
-    return API_URL + "/api/v3/meals.json?guid=" + "a0650549"; //FIXME WHY I gotta put .json
+    return API_URL + "/api/v3/meals.json?guid=" + guid; //FIXME WHY I gotta put .json
   },
   fetchFullReicpe: function(guid: string ){
     fetch(this.getRecipeEndpoint(guid), this.getFetchOptions())
@@ -38,18 +41,34 @@ var RecipeCard = React.createClass({
       .then((responseData) => {
         this.setState({
           loading: false,
-          ingredientDataSource: this.getIngredientDataSource(responseData[0].recipes[0].recipe_foods)
+          prepNotesDataSource: this.getPrepNotesDataSource(responseData[0].recipes[0].prep_notes),
+          ingredientDataSource: this.getIngredientDataSource(responseData[0].recipes[0].recipe_foods),
         })
       })
       .done()
   },
   renderIngredient: function(ingredient: Object) {
     return (
-      <Text>{ingredient.food.name}</Text>
+      <Text style={styles.ingredient}>{entities.decodeHTML(ingredient.food.name)}</Text>
+    )
+  },
+  renderPrepNote: function(
+    prepNote: Object,
+    sectionID: number | string,
+    rowID: number | string,
+    highlightRowFunc: (sectionID: ?number | string, rowID: ?number | string) => void,
+  ) {
+    return (
+      <View>
+        <Text style={styles.ingredient}>{parseInt(rowID) + 1}:{' '}{entities.decodeHTML(prepNote.action)}</Text>
+      </View>
     )
   },
   getIngredientDataSource: function(ingredients: Array<any>) {
     return this.state.ingredientDataSource.cloneWithRows(ingredients);
+  },
+  getPrepNotesDataSource: function(prepNotes: Array<any>) {
+    return this.state.prepNotesDataSource.cloneWithRows(prepNotes);
   },
   getFetchOptions: function(){
     return {
@@ -69,14 +88,26 @@ var RecipeCard = React.createClass({
             style={styles.image}/>
         </View>
         <View>
-          <Text style={styles.recipeName}>{this.props.recipe.name}</Text>
+          <Text style={styles.recipeName}>
+            {entities.decodeHTML(this.props.recipe.name)}
+          </Text>
         </View>
+        <Text style={styles.header}>
+          Ingredients:
+        </Text>
         <ListView
           dataSource={this.state.ingredientDataSource}
           renderRow={this.renderIngredient}
           showsVerticalScrollIndicator={false}
-          scrollEnabled={false}
-        />
+          scrollEnabled={false}/>
+        <Text style={styles.header}>
+          Preparation Instructions:
+        </Text>
+        <ListView
+          dataSource={this.state.prepNotesDataSource}
+          renderRow={this.renderPrepNote}
+          showsVerticalScrollIndicator={false}
+          scrollEnabled={false} />
       </ScrollView>
     )
   }
@@ -92,6 +123,16 @@ var styles = StyleSheet.create({
     flex: 1,
     alignItems: 'stretch',
   },
+  header: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 20,
+    padding: 5
+  },
+  ingredient: {
+    paddingLeft: 5
+  },
   image: {
     flex: 1,
     width: width,
@@ -105,6 +146,10 @@ var styles = StyleSheet.create({
     flex: 1,
     fontSize: 16,
     fontWeight: '500',
+    color: "#fff",
+    backgroundColor: 'rgba(0,0,0,.5)',
+    marginTop: -78,
+    padding: 5
   },
   rating: {
     marginTop: 10,
